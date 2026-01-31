d1db2db4701f947d4cfe3d61ea529a6a383c8785
Rewind8270 <UpToDateGit7020@outlook.com>
2026-01-31 17:32:51 +0900

    Compatible with macOS

--- src/main/java/com/local/PlotPointCloud.java
+++ src/main/java/com/local/PlotPointCloud.java
@@ -29,7 +29,7 @@ public class PlotPointCloud {
         GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE);
-        long window = GLFW.glfwCreateWindow(800 /* width */, 600 /* height */, "HelloGL", 0, 0);
+        long window = GLFW.glfwCreateWindow(800 /* width */, 800 /* height */, "HelloGL", 0, 0);
         GLFW.glfwMakeContextCurrent(window);
         GLFW.glfwSwapInterval(1);
         GLFW.glfwShowWindow(window);
